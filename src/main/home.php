6253f6e9bdd0c181b34e2d12be717e1dc7c1db5b
SIGUI Kessé Emmanuel <devops@sikessem.com>
2021-11-14 15:59:49 +0100

    :tada: Begin the FunCooly website

---
+++ src/main/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta http-equiv="Content-Language" content="fr-FR"/>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <base href="/"/>
+        <title>FunCooly</title>
+    </head>
+    <body>
+        <main>
+            <p>Welcome to FunCooly!</p>
+        </main>
+    </body>
+</html>
